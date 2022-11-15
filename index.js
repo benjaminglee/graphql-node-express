1f57c27e7a0d11f65767e6622f66bc2bb3cc4210
Benjamin Lee <benjaminlee0727@gmail.com>
2022-11-15 17:46:58 -0600

    graphql dummy schema

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const expressGraphQL = require("express-graphql").graphqlHTTP;
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+
+const app = express();
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "HelloWorld",
+    fields: () => ({
+      message: { type: GraphQLString, resolve: () => "Hello World" },
+    }),
+  }),
+});
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema: schema,
+    graphiql: true,
+  })
+);
+app.listen(5000, () => console.log("running server"));
